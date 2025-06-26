37570fa6df5a869462fbc3f399fbe7dafe497d1d
bluefever3 <83917710+bluefever3@users.noreply.github.com>
2025-06-26 16:58:20 +0300

    Delete resource.h

--- resource.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define IDD_MAIN_DIALOG       101
-#define IDC_RADIO_FULLSCREEN  1001
-#define IDC_RADIO_REGION      1002
-#define IDOK                  1
-#define IDCANCEL              2
